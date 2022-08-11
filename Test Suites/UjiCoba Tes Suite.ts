a4279bd5b91bf7e3060bf90c250751b07bbd7433
Anisa Zelkia <zelkiaanisa@gmail.com>
2022-08-12 00:59:58 +0700

    Katalon First Commit

---
+++ Test Suites/UjiCoba Tes Suite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>UjiCoba Tes Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>d5caf231-33e6-4309-9823-e5f54dbf25d8</testSuiteGuid>
+   <testCaseLink>
+      <guid>00f42609-4a7a-401f-9442-cc33d545481e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UjiCoba</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
